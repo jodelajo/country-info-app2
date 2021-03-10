c98b99b1716e412d8cee8ca8c6f7b67244bf7e26
jodelajo <jodelajo@gmail.com>
2021-03-10 15:31:05 +0100

    destructuring object

--- main.js
+++ main.js
@@ -1,15 +1,22 @@
 //Maak een 'Zoek'-knop op de pagina en koppel deze aan een functie die de gegevens over België ophaalt en dit in de console logt.
 // Tip: Als de de documentatie bekijkt en op async zoekt, vindt je een voorbeeld van een GET-request.
 
+//Maak op basis van de response de volgende string en log dit in de console:
+// [country-naam] is situated in [subarea-name]. It has a population of [amount] people.
+
 async function fetchData() {
+
     try {
         const country = 'Belgium'
         const result = await axios.get('https://restcountries.eu/rest/v2/name/Belgium?fullText=true');
-        console.log(result);
-        const response = result.data;
-        console.log(response)
-        const countries = response[0].name
-        console.log(countries)
+        const response = result.data
+        const countries = response[0]
+        const { name, capital, subregion, population } = countries
+        console.log(name, capital, subregion, population)
+        const infoString = `${name} is situated in ${subregion}. It has a population of ${population} people.`
+        console.log(infoString)
+
+
     } catch(e) {
         console.error(e);
     }
